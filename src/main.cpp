622e4bc8b9ca811239c80784e765c8d465d93982
Tom Day <tday@integral.com.au>
2022-09-06 17:26:38 +1000

    setup benchmark

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,58 @@
 #include <iostream>
 #include <string>
+#include <chrono>
 #include "../headers/Simulation.h"
 #include "../headers/Output.h"
 
 int main() {  
-    double SIM_TIME = 10.0f;
+    double SIM_TIME = 5.0f;
     double TIMESTEP_SIZE = 0.00005f;
-    double currentTime = 0.0f;
 
     int OUTPUT_FRAME_RATE = 30;
     int sampleEvery = ( ( static_cast<int> (1/TIMESTEP_SIZE)) / OUTPUT_FRAME_RATE);
-    
-    // Setup sim
-    Simulation *sim = new Simulation(500, TIMESTEP_SIZE);
-    Output *logger = new Output();
 
-    // run sim main
-    int stepCounter = 0;
+    for (double NUM_PARTICLES = 200; NUM_PARTICLES <= 1000; NUM_PARTICLES+=50){
 
-    // Sim loop
-    while (currentTime < SIM_TIME) {
-        
-        // run simulation
-        sim->step();
+        // Using time point and system_clock
+        std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
 
-        if (stepCounter % sampleEvery == 0){
-            logger->log_visualise(sim);
-            logger->log_cg(sim);
+        for(int i = 0; i<5; i++){
+
+            double currentTime = 0.0f;
+
+            // Setup sim
+            Simulation *sim = new Simulation(NUM_PARTICLES, TIMESTEP_SIZE);
+            Output *logger = new Output();
+
+            // run sim main
+            int stepCounter = 0;
+
+            // Sim loop
+            while (currentTime < SIM_TIME) {
+                
+                // run simulation
+                sim->step();
+
+                if (stepCounter % sampleEvery == 0){
+                    logger->log_visualise(sim);
+                    logger->log_cg(sim);
+                }
+
+                currentTime += TIMESTEP_SIZE;
+                stepCounter++;
+            }
+
+            // Sim complete
+            //std::cout << "Simulation complete" << std::endl;
         }
 
-        if (stepCounter % (sampleEvery*OUTPUT_FRAME_RATE) == 0){
-            std::cout << "Completed " << currentTime << "s" << std::endl;
-        }
+        std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+  
+        std::chrono::seconds elapsed_seconds = std::chrono::duration_cast<std::chrono::seconds>(end - start);
 
-        currentTime += TIMESTEP_SIZE;
-        stepCounter++;
+        std::cout << "Five iterations of " << NUM_PARTICLES << " particles processed in" << elapsed_seconds.count() << "s\n";
+
     }
-
-    // Sim complete
-    std::cout << "Simulation complete" << std::endl;
 
     return 0;
 }
